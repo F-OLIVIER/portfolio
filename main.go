3b9573546cc982aacff79f188c827da008e83b7b
folivier <fabien.olivier2@gmail.com>
2024-11-18 10:00:06 +0100

    log

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// Start the HTTP server
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/projet1", handler)
 	http.HandleFunc("/projet2", handler)
@@ -16,6 +18,7 @@ func main() {
 
 	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public/"))))
 	fmt.Println("Server started on port 8080 : http://localhost:8080")
+	logfile("\n______________________________________________________\n___________________Server started_____________________\n______________________________________________________\n")
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -34,9 +37,27 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		adress = "./public/templates/index.html"
 	}
 
-	ts, err := template.ParseFiles(adress)
+	logfile(adress)
+
+	ts, err := template.ParseFiles(r.URL.Path)
 	if err != nil {
 		log.Fatal(err)
 	}
 	ts.Execute(w, nil)
+}
+
+func logfile(texttowrite string) {
+	// Open or create the log file
+	logFile, err := os.OpenFile("access.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("Failed to open log file: %v\n", err)
+	}
+
+	// Set the logger to write to the file
+	log.SetOutput(logFile)
+
+	// Log only the path accessed for statistics
+	log.Printf("Page : %s\n", texttowrite)
+
+	logFile.Close()
 }
